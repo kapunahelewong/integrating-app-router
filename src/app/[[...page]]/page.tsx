4f830f34ca473681152ad804dd854561a6a23537
Kapunahele Wong <kapunahelewong@gmail.com>
2023-06-02 14:05:41 -0700

    add comments about apikey

--- src/app/[[...page]]/page.tsx
+++ src/app/[[...page]]/page.tsx
@@ -1,34 +1,69 @@
-// // For pages and sections, goes in src/app/[[...page]/page.tsx]
+// For PAGES, goes in src/app/[[...page]/page.tsx]
+
+import { builder } from "@builder.io/sdk";
+import Head from "next/head";
+import { RenderBuilderContent } from "@/components/builder";
+
+// Replace with your Public API Key
+// For pages, and sections this must be here or you'll get an API key null error.
+builder.init("2b5ffc858d74425485135b88d2fc307a");
+
+// Define the expected shape of the props
+// object passed to the Page function
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+// Async function called Page takes a single
+// argument called props of type PageProps
+export default async function Page(props: PageProps) {
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get("page", {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
+      // Set prerender to false to prevent infinite rendering loops
+      prerender: false,
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      <Head>
+        <title>{content?.data.title}</title>
+      </Head>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} />
+    </>
+  );
+}
+
+// SECTION - this returns a 404 with a generated app
+
+// import React from "react";
 // import { builder } from "@builder.io/sdk";
 // import Head from "next/head";
 // import { RenderBuilderContent } from "@/components/builder";
 
 // // Replace with your Public API Key
-// // For pages, this must be here or you'll get an API key null error
 // builder.init("2b5ffc858d74425485135b88d2fc307a");
 
-// // Define the expected shape of the props
-// // object passed to the Page function
 // interface PageProps {
 //   params: {
 //     page: string[];
 //   };
 // }
 
-// // Async function called Page takes a single
-// // argument called props of type PageProps
 // export default async function Page(props: PageProps) {
 //   const content = await builder
-//     // Get the page content from Builder with the specified options
-//     .get("page", {
-//       userAttributes: {
-//         // Use the page path specified in the URL to fetch the content
-//         urlPath: "/" + (props?.params?.page?.join("/") || ""),
-//       },
-//       // Set prerender to false to prevent infinite rendering loops
+//     .get("announcement-bar", {
 //       prerender: false,
 //     })
-//     // Convert the result to a promise
 //     .toPromise();
 
 //   return (
@@ -42,10 +77,12 @@
 //   );
 // }
 
-// For links, goes in src/app/[[...page]/page.tsx]. Comment out the above code.
-"use client";
-import Home from "../links/page";
+// NAV LINK
 
-export default function HomePage() {
-  return <Home />;
-}
+// For links, goes in src/app/[[...page]/page.tsx]. Comment out the above code.
+// "use client";
+// import Home from "../links/page";
+
+// export default function HomePage() {
+//   return <Home />;
+// }
